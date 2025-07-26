172ae8dc9cfe3ce87128f509868ab25019bcad99
Sean Bautista <spb762@mail.usask.ca>
2025-07-26 04:52:33 -0600

    Implemented a database and its functions. Also modified features of the bots.

---
+++ dbhandler.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+class database:
+    def __init__(self, fileName):
+        self.con = sqlite3.connect(fileName)
+        self.cur = self.con.cursor()
+        try:
+            self.cur.execute("CREATE TABLE food(name UNIQUE)")
+        except:
+            pass
+
+    def addFood(self, name):
+        try:
+            self.cur.execute(f"INSERT INTO food VALUES(?)", [name])
+            self.con.commit()
+        except:
+            pass
+    
+    def getAllFood(self):
+        return self.cur.execute("SELECT name FROM food").fetchall()
+    
+
+# db1 = database("food.db")
+# db1.addFood("fuh")
+# db1.addFood("japabowl")  
+# print(db1.getAllFood())
+# db1.addFood("thien", "Asian")
+# db1.addFood("japabowl", "Asian")
+# db1.addFood("fuh", "Asian")
+# print(db1.getFood("montanas"))
+# print(db1.getAllFood())
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import discord
+import dbhandler as dh
 from discord.ext import commands
 import logging 
 from dotenv import load_dotenv
 import os
 from random import randint as rd
 
+# Initialize the database
+dbFood = dh.database('Food.db')
+
+# initialize the discord bot and details
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
@@ -12,14 +17,12 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
-
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-#print if the 
+#print after the bot is online on discord
 @bot.event
 async def on_ready():
     print(f"\'{bot.user.name}\' bot is on and ready")
-
 
 #Send a message to whoever says gay
 @bot.event
@@ -33,16 +36,25 @@ async def on_message(message):
 
     await bot.process_commands(message)
 
-#pick where to eat
+#pick where to eat by randomizing it
 @bot.command()
-async def pick(ctx, *, arg):
-    foods = arg.split(',')
-    await ctx.send(foods[rd(0, len(foods)-1)])
+async def pickForMe(ctx):
+    placeToEat = [x[0] for x in dbFood.getAllFood()]
+    if len(placeToEat) == 0:
+        await ctx.send(f"{ctx.author.mention} Add place to eat first!")
+    else:
+        await ctx.send(placeToEat[rd(0, len(placeToEat)-1)])
 
-
-#TODO add place to eat
+#send the list of all the restaurants
 @bot.command()
+async def getAll(ctx):
+    await ctx.send([x[0] for x in dbFood.getAllFood()])
 
-
+#Add places to eat from
+@bot.command()
+async def addResto(ctx, *, arg):
+    for a in arg.split(', '):
+        dbFood.addFood(a.lower());
+    await ctx.send(f"{ctx.author.mention} added {arg} to the collection!")
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
