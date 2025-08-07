9be187b9eb14cebf21e9e8b74c1c7ca4ec8ae482
Sean Bautista <spb762@mail.usask.ca>
2025-08-07 02:23:56 -0600

    Changed the main.py to accomodate discord cogs

--- main.py
+++ main.py
@@ -1,67 +1,52 @@
-import discord
-import dbhandler as dh
 from discord.ext import commands
-import logging 
 from dotenv import load_dotenv
+from dbhandler import database as db
+import discord
+import logging
 import os
-from random import randint as rd
+import asyncio
 
-# Initialize the database
-dbFood = dh.database('Food.db')
+"""    
+Class Construct for Bot    
+"""
+class Bot:
+    def __init__(self, fName: str):
+        self.handler = logging.FileHandler(filename=fName, encoding='utf-8', mode='w')
+        self.intents = discord.Intents.default()
+    
+    def configure(self):
+        self.intents.message_content = True
+        self.intents.members = True
 
-# initialize the discord bot and details
+    def getIntents(self):
+        return self.intents
+
+
+# Init discord Token
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+# Init database
+dbFood = db('Food.db')
 
-#print after the bot is online on discord
+#init the discord bot and Configurations
+botConfig = Bot('discord.log')
+botConfig.configure()
+
+bot = commands.Bot(command_prefix='!', intents=botConfig.getIntents())
+
 @bot.event
 async def on_ready():
-    print(f"\'{bot.user.name}\' bot is on and ready")
+    print(f"{bot.user.name} is logged in.")
 
-#Send a message to whoever says gay
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return 
+async def load():
+    for fname in os.listdir("./cogs"):
+        if fname.endswith(".py"):
+            await bot.load_extension(f"cogs.{fname[:-3]}")
 
-    if "gay" in message.content.lower():
-        print(message.content.lower())
-        await message.channel.send(f"no youre gay {message.author.mention}")
+async def main():
+    async with bot:
+        await load()
+        await bot.start(token)
 
-    elif "test" in message.content.lower():
-        embedMsg = discord.Embed(title="Title",
-                                 description="This is the description",
-                                 color=0x2C2F33)
-        embedMsg.add_field(name="something", value="else", inline=False)
-        await message.channel.send(embed=embedMsg)
-
-    await bot.process_commands(message)
-
-#pick where to eat by randomizing it
-@bot.command()
-async def pickForMe(ctx):
-    placeToEat = [x[0] for x in dbFood.getAllFood()]
-    if len(placeToEat) == 0:
-        await ctx.send(f"{ctx.author.mention} Add place to eat first!")
-    else:
-        await ctx.send(placeToEat[rd(0, len(placeToEat)-1)])
-
-#send the list of all the restaurants
-@bot.command()
-async def getAll(ctx):
-    await ctx.send([x[0] for x in dbFood.getAllFood()])
-
-#Add places to eat from
-@bot.command()
-async def addResto(ctx, *, arg):
-    for a in arg.split(', '):
-        dbFood.addFood(a.lower());
-    await ctx.send(f"{ctx.author.mention} added {arg} to the collection!")
-
-bot.run(token, log_handler=handler, log_level=logging.DEBUG)
+asyncio.run(main())
